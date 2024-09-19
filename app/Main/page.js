a19d74ee010898e32b50cba7bc1264ef877075ef
noorverma <vermanoor89@gmail.com>
2024-09-19 17:41:54 -0600

    main page changes

--- app/Main/page.js
+++ app/Main/page.js
@@ -6,14 +6,14 @@ const MainPage = () => {
       <Navbar />
       <header
         className="relative flex items-center justify-center h-screen bg-cover bg-center"
-        style={{ backgroundImage: "url('/backgroundMain.jpg')" }} // Add your image URL here
+        style={{ backgroundImage: "url('/backgroundMain.jpg')" }}
       >
         <div className="absolute inset-0 bg-gray-900 opacity-60"></div>
         <div className="relative text-center z-10">
           <h1 className="text-5xl font-bold text-orange-500 mb-4">
             Your Vision Wired to Perfection
           </h1>
-          <p className="text-gray-600 mb-8">
+          <p className="text-white mb-8">
             Delivering Advanced Electrical Engineering Solutions to Power your Success.
           </p>
           <div className="flex justify-center space-x-4">
